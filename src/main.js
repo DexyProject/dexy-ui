e67881bd653cae23daed57cec34fa043ce12024a
Ivo Georgiev <ivo@linvo.org>
2018-01-04 02:27:48 +0200

    Tabs

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-angular.module('dexyApp', ['lumx'])
+var app = angular.module('dexyApp', ['lumx'])
+
+// Constants
+app.run(['$rootScope', function($rootScope) {
+	$rootScope.tabs = [
+		{ name: 'Markets', go: 'markets' },
+		{ name: 'Wallets', go: 'wallets' }
+	]
+}])
+
+// ...
