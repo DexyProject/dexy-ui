ba46ca5f1a7f3fc29dff658ef64ae1959b5d133b
Ivo Georgiev <ivo@linvo.org>
2018-01-04 04:15:39 +0200

    Test highcharts

--- src/pages/exchange/exchange.js
+++ src/pages/exchange/exchange.js
@@ -2,6 +2,11 @@
 {
     'use strict';
 
+    // XXX NOTE
+    // Using a directive for charts considered harmful
+    // We will have to update the charts often, and often just a small portion (e.g. inserting a new bar)
+    // Directives will require angular to re-render the whole page, which is not efficient
+
     angular
         .module('dexyApp')
         .controller('marketCtrl', marketCtrl);
@@ -14,5 +19,37 @@
 
 
         exchange.pair = $stateParams.pair
+
+        $.getJSON('https://www.highcharts.com/samples/data/jsonp.php?a=e&filename=aapl-ohlc.json&callback=?', function (data) {
+
+            // create the chart
+            Highcharts.stockChart('candleChart', {
+                rangeSelector: {
+                    selected: 1
+                },
+
+                title: {
+                    text: 'AAPL Stock Price'
+                },
+
+                series: [{
+                    type: 'candlestick',
+                    name: 'AAPL Stock Price',
+                    data: data,
+                    dataGrouping: {
+                        units: [
+                            [
+                                'week', // unit name
+                                [1] // allowed multiples
+                            ], [
+                                'month',
+                                [1, 2, 3, 4, 6]
+                            ]
+                        ]
+                    }
+                }]
+            });
+        });
+
     }
 })();
