b9c3b5c6ad1f73b420de63d59de0a3c8cf5e6ed9
Ivo Georgiev <ivo@linvo.org>
2018-01-06 17:16:20 +0200

    Markets: show fiat amounts, dummy data

--- src/components/markets/markets.js
+++ src/components/markets/markets.js
@@ -6,9 +6,9 @@
         .module('dexyApp')
         .controller('MarketsController', MarketsController);
 
-    MarketsController.$inject = ['$filter', '$scope', '$state'];
+    MarketsController.$inject = ['$filter', '$scope', '$state', 'cmc'];
 
-    function MarketsController($filter, $scope, $state)
+    function MarketsController($filter, $scope, $state, cmc)
     {
         var vm = this;
 
@@ -24,31 +24,45 @@
             sortable: true
         },
         {
+            name: 'balance',
+            label: 'Balance',
+            sortable: true,
+        },
+        {
             name: 'price',
             label: 'Price',
             //icon: 'comment-text',
+            format: formatRowWithFIAT.bind(null, 'price'),
             sortable: true
         },
         {
             name: 'change',
             label: '% Change',
+            format: function(row) {
+                return '<div class="'+(row.change >= 0 ? 'positive' : 'negative')+'"">'
+                    +(row.change*100).toFixed(2)
+                    +'%</div>'
+            },
             sortable: true,
         },
         {
             // default sort
             name: 'vol',
             label: '24hr Volume',
+            format: formatRowWithFIAT.bind(null, 'vol'),
             sortable: true,
             sort: 'desc'
         },
         {
-            name: '24hr_high',
+            name: 'high',
             label: '24hr High',
+            format: formatRowWithFIAT.bind(null, 'high'),
             sortable: true,
         },
         {
-            name: '24hr_low',
+            name: 'low',
             label: '24hr Low',
+            format: formatRowWithFIAT.bind(null, 'low'),
             sortable: true,
         },
 
@@ -71,6 +85,15 @@
         //vm.dataTableTbody = angular.copy(CONSTS.markets);
         // ugly but works
         var markets = angular.copy(CONSTS.markets)
+        markets.forEach(function(x) {
+            // TEMP TMP TEMP 
+            x.price = Math.random()
+            x.high = x.price + Math.random()*0.2
+            x.low = x.price - Math.random()*0.2
+            x.vol = 200 * Math.random()
+            x.change = 0.4*(Math.random()-0.5)
+            x.balance = 0
+        })
 
         // debounce the search?
         $scope.$watch('hideZeroBal', updateItems)
@@ -102,6 +125,17 @@
         {
             vm.dataTableTbody = $filter('orderBy')
                 (vm.dataTableTbody, _column.name, _column.sort === 'desc' ? true : false)
+        }
+
+        function formatRowWithFIAT(prop, row)
+        {
+            // TODO: use angular number format 
+            
+            var fiatAmnt = $scope.useEUR
+            ? (row[prop] * cmc.pairs.ETHEUR).toFixed(2) + '€'
+            : (row[prop] * cmc.pairs.ETHUSD).toFixed(2) + '$'
+
+            return '<div>'+row[prop].toFixed(8)+'</div><div class="label">'+fiatAmnt+'</div>'
         }
     }
 })();
