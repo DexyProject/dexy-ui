862304f50707ba56f4bbf142eae07abd9eb54918
decanus <dean.eigenmann@icloud.com>
2018-01-11 15:46:38 +0100

    minor reworking of interface, changed image display size

--- src/components/markets/markets.js
+++ src/components/markets/markets.js
@@ -73,7 +73,7 @@
             name: 'image',
             format: function(row)
             {
-                return '<img src="img/markets/'+row.symbol+'-ETH.png" height=40></a>';
+                return '<img src="img/markets/'+row.symbol+'-ETH.png" height=30></a>';
             }
         });
 
