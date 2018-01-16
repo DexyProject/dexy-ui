1956f1487f55792d8793eada405eb93d5420e8b3
Dean Eigenmann <dean@tokenate.io>
2018-01-16 13:19:00 +0100

    Update exchange.js

--- src/components/exchange/exchange.js
+++ src/components/exchange/exchange.js
@@ -82,146 +82,177 @@
 
             prices.reverse()
             volume.reverse()
+Highcharts.setOptions({
+      lang: {
+        rangeSelectorZoom: ''
+      }
+    });
 
-            Highcharts.stockChart('mainChart', {
-            rangeSelector: {
-              inputEnabled: false,
-              selected: 1,
-              buttons: [{
-                type: 'hour',
-                count: 1,
-                text: '1h'
-              }, {
-                type: 'day',
-                count: 1,
-                text: '1d'
-              }, {
-                type: 'week',
-                count: 1,
-                text: '1w'
-              }, {
-                type: 'month',
-                count: 1,
-                text: '1m'
-              }, ],
-            },
-            plotOptions: {
-              candlestick: {
-                color: '#e57373',
-                lineColor: '#e57373',
-                upColor: 'transparent',
-                upLineColor: '#4db6ac'
+    Highcharts.stockChart('mainChart', {
+      rangeSelector: {
+        inputEnabled: false,
+        selected: 1,
+        buttons: [{
+          type: 'hour',
+          count: 1,
+          text: '1h'
+        }, {
+          type: 'day',
+          count: 1,
+          text: '1d'
+        }, {
+          type: 'week',
+          count: 1,
+          text: '1w'
+        }, {
+          type: 'month',
+          count: 1,
+          text: '1m'
+        }],
+        buttonTheme: {
+          fill: 'none',
+          stroke: 'none',
+          'stroke-width': 0,
+          style: {
+            color: '#b9b9b9',
+            textTransform: 'uppercase',
+          },
+          states: {
+            hover: {
+              fill: 'none',
+              stroke: 'none',
+              'stroke-width': 0,
+              style: {
+                color: '',
               }
             },
-            chart: {
-              height: '44%',
-              panning: true,
-            },
-            credits: {
-              enabled: false
-            },
-            navigator: {
-              enabled: false
-            },
-            scrollbar: {
-              enabled: false
-            },
-            title: {
-              enabled: false
-            },
-            yAxis: [{
-              crosshair: {
-                dashStyle: 'Dot',
-                snap: false,
-                label: {
-                	enabled: true,
-                	format: '{value:.5f}'
-                }
-              },
-              height: '80%',
-              lineWidth: 0,
-              min: 0,
-              gridLineWidth: 0,
-              lineColor: '#b9b9b9',
-              offset: 0,
-              labels: {
-                align: 'left',
-                style: {
-                  color: '#b9b9b9',
-                  'min-width': '40px'
-                },
+            select: {
+              fill: 'none',
+              stroke: 'none',
+              'stroke-width': 0,
+              style: {
+                color: '#6c54c9',
               }
-            }, {
-              crosshair: {
-                dashStyle: 'Dot',
-                snap: false,
-              },
-              top: '80%',
-              height: '20%',
-              lineWidth: 0,
-              offset: 0,
-              gridLineWidth: 0,
-              lineColor: '#b9b9b9',
-              labels: {
-                enabled: false
-              }
-            }],
-            xAxis: [{
-              dateTimeLabelFormats: {
-                day: '%b %e',
-                week: '%b %e',
-                month: '%b \'%y',
-              },
-              crosshair: {
-                dashStyle: 'Dot',
-                snap: false,
-                label: {
-                	enabled: true
-                }
-              },
-              ordinal: false,
-              labels: {
-                style: {
-                  color: '#b9b9b9'
-                },
-              }
-            }],
-            tooltip: {
-              enabled: false
-            },
-            series: [{
-              type: 'candlestick',
-              name: 'Price',
-              data: prices,
-              dataGrouping: {
-                enabled: true,
-                forced: true,
-                groupPixelWidth: 25,
+            }
+          }
+        },
+      },
+      plotOptions: {
+        candlestick: {
+          color: '#e57373',
+          lineColor: '#e57373',
+          upColor: 'transparent',
+          upLineColor: '#4db6ac'
+        }
+      },
+      chart: {
+        height: '44%',
+        panning: true,
+      },
+      credits: {
+        enabled: false
+      },
+      navigator: {
+        enabled: false
+      },
+      scrollbar: {
+        enabled: false
+      },
+      title: {
+        enabled: false
+      },
+      yAxis: [{
+        crosshair: {
+          dashStyle: 'Dot',
+          snap: false,
+          label: {
+            enabled: true,
+            format: '{value:.5f}'
+          }
+        },
+        height: '80%',
+        lineWidth: 0,
+        min: 0,
+        gridLineWidth: 0,
+        lineColor: '#b9b9b9',
+        offset: 0,
+        labels: {
+          align: 'left',
+          style: {
+            color: '#b9b9b9',
+            'min-width': '40px'
+          },
+        }
+      }, {
+        crosshair: {
+          dashStyle: 'Dot',
+          snap: false,
+        },
+        top: '80%',
+        height: '20%',
+        lineWidth: 0,
+        offset: 0,
+        gridLineWidth: 0,
+        lineColor: '#b9b9b9',
+        labels: {
+          enabled: false
+        }
+      }],
+      xAxis: [{
+        dateTimeLabelFormats: {
+          day: '%b %e',
+          week: '%b %e',
+          month: '%b \'%y',
+        },
+        crosshair: {
+          dashStyle: 'Dot',
+          snap: false,
+          label: {
+            enabled: true
+          }
+        },
+        ordinal: false,
+        labels: {
+          style: {
+            color: '#b9b9b9'
+          },
+        }
+      }],
+      tooltip: {
+        enabled: false
+      },
+      series: [{
+        type: 'candlestick',
+        name: 'Price',
+        data: prices,
+        dataGrouping: {
+          enabled: true,
+          forced: true,
+          groupPixelWidth: 25,
 
-                units: [
-                  ['hour', [1, 2, 3, 4, 6, 8, 12, 24, 48]]
-                ]
-              }
-            }, {
-              type: 'column',
-              name: 'Volume',
-              pointWidth: 10,
-              data: volume,
-              yAxis: 1,
-              dataGrouping: {
-                enabled: true,
-                forced: true,
-                groupPixelWidth: 25,
+          units: [
+            ['hour', [1, 2, 3, 4, 6, 8, 12, 24, 48]]
+          ]
+        }
+      }, {
+        type: 'column',
+        name: 'Volume',
+        pointWidth: 10,
+        data: volume,
+        yAxis: 1,
+        dataGrouping: {
+          enabled: true,
+          forced: true,
+          groupPixelWidth: 25,
 
-                units: [
-                  ['hour', [1, 2, 3, 4, 6, 8, 12, 24, 48]]
-                ]
-              },
-              color: '#b9b9b9'
-            }],
-          });
-
-        });
+          units: [
+            ['hour', [1, 2, 3, 4, 6, 8, 12, 24, 48]]
+          ]
+        },
+        color: '#b9b9b9'
+      }],
+    });
+           
 
         });
 
