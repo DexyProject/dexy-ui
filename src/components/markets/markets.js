c55a559bfb331ee5424e3fd8a2f99806a1677114
decanus <dean.eigenmann@icloud.com>
2018-01-31 13:58:30 +0100

    Updated

--- src/components/markets/markets.js
+++ src/components/markets/markets.js
@@ -6,99 +6,38 @@
         .module('dexyApp')
         .controller('MarketsController', MarketsController);
 
-    MarketsController.$inject = ['$filter', '$scope', '$state', 'cmc', 'user'];
+    MarketsController.$inject = ['$scope', '$state', 'cmc', 'user'];
 
-    function MarketsController($filter, $scope, $state, cmc, user)
+    function MarketsController($scope, $state, cmc, user)
     {
-        var vm = this;
+        $scope.orderByField = 'vol';
+        $scope.reverseSort = true;
+        $scope.searchKeyword = '';
+        $scope.hideZeroBal = false;
+        $scope.persistingProp($scope, 'hideZeroBal');
 
-        vm.dataTableThead = [
-        {
-            name: 'symbol',
-            label: 'Market',
-            sortable: true
-        },
-        {
-            name: 'name',
-            label: 'Name',
-            sortable: true
-        },
-        {
-            name: 'balance',
-            label: 'Balance',
-            sortable: true,
-            format: function(row) { return row.balance.toFixed(2) }
-        },
-        {
-            name: 'price',
-            label: 'Price',
-            //icon: 'comment-text',
-            format: formatRowWithFIAT.bind(null, 'price'),
-            sortable: true
-        },
-        {
-            name: 'change',
-            label: '% Change',
-            format: function(row) {
-                return '<div class="'+(row.change >= 0 ? 'positive' : 'negative')+'"">'
-                    +(row.change*100).toFixed(2)
-                    +'%</div>'
-            },
-            sortable: true,
-        },
-        {
-            // default sort
-            name: 'vol',
-            label: '24h Volume',
-            format: formatRowWithFIAT.bind(null, 'vol'),
-            sortable: true,
-            sort: 'desc'
-        },
-        {
-            name: 'high',
-            label: '24h High',
-            format: formatRowWithFIAT.bind(null, 'high'),
-            sortable: true,
-        },
-        {
-            name: 'low',
-            label: '24h Low',
-            format: formatRowWithFIAT.bind(null, 'low'),
-            sortable: true,
-        },
+        $scope.openExchange = function (symbol) {
+            $state.go('exchange', { pair: symbol })
+        };
 
-        ];
-        vm.advancedDataTableThead = angular.copy(vm.dataTableThead);
-        vm.advancedDataTableThead.unshift(
-        {
-            name: 'image',
-            format: function(row)
-            {
-                return '<img src="img/markets/'+row.symbol+'-ETH.png" height=30></a>';
-            }
-        });
-
-        // Persistent props
-        $scope.hideZeroBal = false
-        $scope.persistingProp($scope, 'hideZeroBal')
 
         // Fill in vol, price_eth, price_fiat
         //vm.dataTableTbody = angular.copy(CONSTS.markets);
         // ugly but works
-        var markets = angular.copy(CONSTS.markets)
-        markets.forEach(function(x) {
-            // TEMP TMP TEMP 
+        $scope.markets = angular.copy(CONSTS.markets)
+        $scope.markets.forEach(function(x) {
+            // TEMP TMP TEMP
             x.price = Math.random()
             x.high = x.price + Math.random()*0.2
             x.low = x.price - Math.random()*0.2
             x.vol = 200 * Math.random()
             x.change = 0.4*(Math.random()-0.5)
             x.balance = 0
-            
+
             x.token = CONSTS.tokens[x.symbol]
         })
 
-        // NOTE: this is very similar to the code in exchange.js, maybe make it more abstract 
+        // NOTE: this is very similar to the code in exchange.js, maybe make it more abstract
         // once we have to get from the contract too
         $scope.$watch(function () {
             return user.publicAddr
@@ -109,7 +48,7 @@
 
             console.log('Fetching all balances for '+addr)
 
-            markets.forEach(function(x) {
+            $scope.markets.forEach(function(x) {
                 if (!x.token) return
 
                 //console.log('Fetching ' + x.symbol + ' balances for ' + addr)
@@ -118,7 +57,7 @@
                     if (err) console.error(err)
                     else {
                         x.balance = bal / x.token[1]
-                        if (!$scope.$$phase) $scope.$apply(function() { updateItems() })
+                        if (!$scope.$$phase) $scope.$apply(function() { })
                     }
                 }))
             })
@@ -126,47 +65,8 @@
             batch.execute()
         })
 
-        // debounce the search?
-        $scope.$watch('hideZeroBal', updateItems)
-        $scope.$watch('vm.search', updateItems)
-
-        function updateItems() {
-            vm.dataTableTbody = [].concat(markets)
-            
-            if ($scope.hideZeroBal) vm.dataTableTbody = vm.dataTableTbody.filter(function(x) {
-                return x.balance > 0
-            })
-
-            if (vm.search) vm.dataTableTbody = $filter('filter')(vm.dataTableTbody, vm.search)
-        }
-
-        $scope.$on('lx-data-table__selected', updateActions);
-        $scope.$on('lx-data-table__unselected', updateActions);
-        $scope.$on('lx-data-table__sorted', updateSort);
-
-        ////////////
-
-        function updateActions(_event, _dataTableId, _selectedRows)
-        {
-            $state.go('exchange', { pair: _selectedRows[0].symbol })
-
-        }
-
-        function updateSort(_event, _dataTableId, _column)
-        {
-            vm.dataTableTbody = $filter('orderBy')
-                (vm.dataTableTbody, _column.name, _column.sort === 'desc' ? true : false)
-        }
-
-        function formatRowWithFIAT(prop, row)
-        {
-            // TODO: use angular number format 
-
-            var fiatAmnt = $scope.useEUR
-            ? '€'+(row[prop] * cmc.pairs.ETHEUR).toFixed(2)
-            : '$'+(row[prop] * cmc.pairs.ETHUSD).toFixed(2)
-
-            return '<div>'+row[prop].toFixed(8)+'</div><div class="label">'+fiatAmnt+'</div>'
+        $scope.fiatValue = function (value) {
+            return $scope.useEUR ? '€'+(value * cmc.pairs.ETHEUR).toFixed(2) : '$'+(value * cmc.pairs.ETHUSD).toFixed(2)
         }
     }
 })();
