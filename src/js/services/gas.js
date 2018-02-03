1c7305ddf1cca68c7892618990335283e41e005e
Ivo Georgiev <ivo@linvo.org>
2018-02-03 19:57:06 +0200

    Gas: select default

--- src/js/services/gas.js
+++ src/js/services/gas.js
@@ -5,9 +5,9 @@
         .module('dexyApp')
         .service('gas', gasService)
 
-    gasService.$inject = ['$http', '$interval']
+    gasService.$inject = ['$http', '$interval', '$rootScope']
 
-    function gasService($http, $interval) {
+    function gasService($http, $interval, $root) {
         var gas = this;
         var URL = 'https://ethgasstation.info/json/ethgasAPI.json';
 
@@ -16,12 +16,11 @@
         function update() {
             $http.get(URL)
                 .then(function (resp) {
-
-                    console.log(resp.data)
                     gas.prices.fast = (resp.data.fast / 10).toFixed(0)
                     gas.prices.safe = (resp.data.safeLow / 10).toFixed(0)
                     gas.prices.average = (resp.data.average / 10).toFixed(0)
 
+                    if (! $root.gas) $root.gas = gas.prices.average
                 })
         }
 
