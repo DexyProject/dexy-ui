26c2d70d6faa98e6dd558a9ca1d0e58e98a365ef
decanus <dean.eigenmann@icloud.com>
2018-03-07 19:54:09 +0100

    moved files, implemented my trades

--- src/components/exchange/exchange.js
+++ src/components/exchange/exchange.js
@@ -168,6 +168,38 @@
             }
         }
 
+        exchange.mapTransaction = function(tx, i) {
+
+            var amount = parseInt(tx.give.token === CONSTS.ZEROADDR ? tx.get.amount : tx.give.amount) / exchange.tokenInf[1];
+            var side = (tx.give.token === CONSTS.ZEROADDR ? 'buy' : 'sell');
+
+            var time = new Date(tx.timestamp * 1000)
+
+            var pad = function(x) { return ('00'+x).slice(-2) }
+            return {
+                idx: i,
+                tx: tx.tx,
+                time: time.getDate() + '/' + (time.getMonth()+1) + ' ' + pad(time.getHours()) + ':' + pad(time.getMinutes()),
+                side: side,
+                amount: amount,
+                price: calculatePrice(tx)
+            }
+        }
+
+        // NOTE: similar math is used for the orderbook
+        function calculatePrice(o) {
+            var getAmnt = parseInt(o.get.amount)
+            var giveAmnt = parseInt(o.give.amount)
+
+            var tokenAmount = (o.give.token === CONSTS.ZEROADDR ? getAmnt : giveAmnt)
+            var tokenBase = exchange.tokenInf[1]
+
+            var ethAmount = (o.give.token === CONSTS.ZEROADDR ? giveAmnt : getAmnt)
+            var ethBase = 1000000000000000000
+
+            return (ethAmount / ethBase) / (tokenAmount / tokenBase)
+        }
+
         exchange.txError = function(msg, err) 
         {
             console.error(err)
---
+++ src/components/exchange/user/myTrades.js
@@ -0,0 +1,35 @@
+(function () {
+    'use strict';
+
+    // My trades ctrl
+    angular
+        .module('dexyApp')
+        .controller('myTradesCtrl', myTradesCtrl);
+
+    myTradesCtrl.$inject = ['$scope', 'user'];
+
+    function myTradesCtrl($scope, user) {
+
+        var exchange = $scope.exchange
+
+        $scope.$watch(function() { return user.publicAddr }, fetchTrades)
+        $scope.$on('reload-orders', fetchTrades)
+
+        function fetchTrades() {
+            if (!user.publicAddr) return
+
+            fetch(cfg.endpoint + '/trades?token=' + exchange.tokenInf[0] + '&user=' + user.publicAddr)
+                .then(function (res) {
+                    return res.json()
+                })
+                .then(function (ob) {
+                    exchange.myTrades = (ob || []).map(exchange.mapTransaction)
+                    if (!$scope.$$phase) $scope.$digest()
+                })
+                .catch(function (err) {
+                    console.error(err)
+                })
+        }
+
+    }
+})();
