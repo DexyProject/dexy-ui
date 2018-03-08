c2de79485b9624712961f4f3deb169faa92d3781
Ivo Georgiev <ivo@linvo.org>
2018-03-08 13:27:26 +0200

    takeOrder: call canTrade

--- src/components/exchange/takeOrder.js
+++ src/components/exchange/takeOrder.js
@@ -38,7 +38,7 @@
             $('#takeOrder').modal('show')
         }
 
-        $scope.takeOrder = function (toFill) {
+        $scope.getTradeArgs = function (toFill) {
             var rawOrder = toFill.order.order
 
             // addresses - user, tokenGive, tokenGet
@@ -54,8 +54,13 @@
 
             var sig = rawOrder.signature
 
+            return [addresses, values, sig.v, sig.r, sig.s, amnt, sig.sig_mode]
+        }
+
+        $scope.takeOrder = function (toFill) {
             // NOTE: this has to be executed in the same tick as the click, otherwise trezor popups will be blocked
-            var tx = user.exchangeContract.methods.trade(addresses, values, sig.v, sig.r, sig.s, amnt, sig.sig_mode)
+            var tx = user.exchangeContract.methods.trade.apply(null, $scope.getTradeArgs(toFill))
+
             user.sendTx(tx, {from: user.publicAddr, gas: 150 * 1000, gasPrice: user.GAS_PRICE}, function (err, txid) {
                 if (err) return $scope.exchange.txError('Error taking order', err)
 
@@ -66,30 +71,27 @@
 
         // will only get triggered when the reference to the object changes
         // which essentially means it won't double-trigger once we set toFill.canTrade
-        /*
-        $scope.$watch(function() { return $scope.exchange.toFill }, function(toFill) {
-            if (! toFill) return
-            if (! toFill.order) return
-            var rawOrder = toFill.order.order
+        $scope.$watchCollection(function() { 
+            if (! $scope.exchange.toFill) return null
+            return [$scope.exchange.toFill, $scope.exchange.toFill.portion] 
+        }, function() {
+            if (! $scope.exchange.toFill) return
 
-            var addresses = [rawOrder.user, rawOrder.give.token, rawOrder.get.token]
-            var values = [rawOrder.give.amount, rawOrder.get.amount, rawOrder.expires, rawOrder.nonce]
-            var portion = toFill.portion / 1000
-            var amnt = Math.floor(parseInt(rawOrder.get.amount) * portion).toString()
-
-            var sig = rawOrder.signature
-
-            // @TODO: call canTrade, remove order if invalid
-            // @TODO: call isApproved before that, and if it's not, make the user wait. or say "you cannot submit an order yet"
-            //   same goes for filling
+            // @TODO: call isApproved before that, and if it's not, make the user wait. or say "you cannot submit an order yet", same goes for filling
             // NOTE: this has to be shown upon opening the dialog; so the things that getAddresses, values, and amount, should be functions
-            console.log(amnt)
-            user.exchangeContract.methods.canTrade(addresses, values, sig.v, sig.r, sig.s, amnt, sig.sig_mode)
+            var args = $scope.getTradeArgs($scope.exchange.toFill)
+            user.exchangeContract.methods.canTrade.apply(null, args)
                 .call(function (err, resp) {
-                    if (err) toastr.err('Error getting order canTrade status')
-                    else $scope.exchange.toFill.canTrade = resp
+                    if (err) {
+                        toastr.error('Error getting order canTrade status')
+                        return
+                    }
 
-                    console.log(resp)
+                    $scope.exchange.toFill.canTrade = resp
+
+                    if (resp === false) {
+                        console.log('Cannot trade order', $scope.exchange.toFill.order, args)
+                    }
 
                     !$scope.$$phase && $scope.$digest()
                 })
@@ -100,7 +102,7 @@
             //         console.log('didSign', err, resp)
             //     })
         })
-        */
+        
 
         $scope.getSummary = function()
         {
