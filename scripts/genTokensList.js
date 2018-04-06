417a8fe165dc7d24b47e8cf5be1141e5a99d5c4c
Dean Eigenmann <dean@tokenate.io>
2018-04-07 00:41:27 +0200

    Update genTokensList.js

--- scripts/genTokensList.js
+++ scripts/genTokensList.js
@@ -10,6 +10,11 @@ fetch('https://raw.githubusercontent.com/MyEtherWallet/ethereum-lists/master/tok
 .then(function(res) { return res.json() })
 .then(function(res) {
 	res.forEach(function(token) {
+		
+		if (token.symbol == 'EOS' || token.symbol == 'TRX') {
+			return;
+		}
+		
 		tokens[token.symbol] = [token.address, Math.pow(10, parseInt(token.decimals, 10))]
 	})
 
