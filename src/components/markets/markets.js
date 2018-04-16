f310cfcdc04c1a7d16640f1593ba945c38aa316f
Ivo Georgiev <ivo@strem.io>
2018-04-17 00:06:01 +0300

    Sort: icons on order

--- src/components/markets/markets.js
+++ src/components/markets/markets.js
@@ -16,6 +16,16 @@
         $scope.orderByField = 'volume';
         $scope.reverseSort = true;
 
+        $scope.fields = [
+            { name: 'Market', field: 'symbol' },
+            { name: 'Balance On Wallet', field: 'balanceWallet' },
+            { name: 'Balance On Exchange', field: 'balanceExchange' },
+            { name: 'Bid', field: 'bid' },
+            { name: 'Ask', field: 'ask' },
+            { name: 'Last Price', field: 'last' },
+            { name: '24h Volume', field: 'volume' },
+        ]
+
         $scope.hideZeroBal = false;
         $scope.persistingProp($scope, 'hideZeroBal');
 
