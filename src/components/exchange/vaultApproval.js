4ab955a809c565b436aa234ab418df4f0b829f34
Ivo Georgiev <ivo@strem.io>
2018-04-21 18:28:04 +0300

    Basic overlay (not styled)

--- src/components/exchange/vaultApproval.js
+++ src/components/exchange/vaultApproval.js
@@ -9,6 +9,10 @@
     vaultApprovalCtrl.$inject = ['$scope', 'user'];
 
     function vaultApprovalCtrl($scope, user) {
+        // This variable will be false when we're sure it's not approved
+        // It will become true either if it's approved (checkVaultApproval), or the tx to approve is sent
+        $scope.exchange.isVaultApproved = true
+
         $scope.$watch(function () {
             return user.publicAddr
         }, checkVaultApproval)
@@ -20,20 +24,23 @@
                 .call(function (err, isApproved) {
                     if (err) console.error(err)
 
-                    if (isApproved === false) $('#approveExchangeByVault').modal('show')
-                    if (isApproved === true) $('#approveExchangeByVault').modal('hide')
+                    $scope.exchange.isVaultApproved = isApproved
                 })
         }
 
         $scope.approveExchangeByVault = function () {
             var tx = user.vaultContract.methods.approve(cfg.exchangeContract)
 
-             $('#approveExchangeByVault').modal('hide')
+            $('#approveExchangeByVault').modal('hide')
+            
             // @TODO: saner gas limit
             user.sendTx(tx, {from: user.publicAddr, gas: 60 * 1000, gasPrice: user.GAS_PRICE}, function (err, txid) {
                 if (err) return $scope.exchange.txError('Vault approval failed', err)
 
-                if (txid) $scope.exchange.txSuccess(txid)
+                if (txid) {
+                    $scope.exchange.isVaultApproved = true
+                    $scope.exchange.txSuccess(txid)
+                }
             })
         }
     }
