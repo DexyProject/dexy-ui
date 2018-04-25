599339b257ed2350e1ed4b86bbfdce3fbe65c864
decanus <dean.eigenmann@icloud.com>
2018-04-25 19:35:03 +0200

    copying images

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,40 @@
-var gulp          = require('gulp');
-var source        = require('vinyl-source-stream');
-var browserify    = require('browserify');
-var babelify      = require('babelify');
-var ngAnnotate    = require('browserify-ngannotate');
-var browserSync   = require('browser-sync').create();
-var rename        = require('gulp-rename');
+var gulp = require('gulp');
+var source = require('vinyl-source-stream');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var ngAnnotate = require('browserify-ngannotate');
+var browserSync = require('browser-sync').create();
+var rename = require('gulp-rename');
 var templateCache = require('gulp-angular-templatecache');
-var uglify        = require('gulp-uglify');
-var merge         = require('merge-stream');
+var uglify = require('gulp-uglify');
+var merge = require('merge-stream');
 
-var nib = require('nib')
-var stylus = require('gulp-stylus')
+var nib = require('nib');
+var stylus = require('gulp-stylus');
 var pug = require('gulp-pug');
-var concat = require('gulp-concat')
+var concat = require('gulp-concat');
+var path = require('path');
 
 var pack = require('./package.json');
 
 // Where our files are located
-var jsFiles   = "new_src/js/**/*.js";
-var cssFiles   = "new_src/styl/*.styl";
+var jsFiles = "new_src/js/**/*.js";
+var cssFiles = "new_src/styl/*.styl";
 var viewFiles = "new_src/**/*.pug";
 
 
-gulp.task('browserify', ['views'], function() {
+gulp.task('browserify', ['views'], function () {
+
+    gulp.src(['img/*', 'img/markets/*'])
+        .pipe(gulp.dest(function (f) {
+            if (f.path.match('node_modules/')) return path.dirname(f.path.match('dist/(.*)')[0]);
+            return path.join('./dist', path.dirname(path.relative(process.cwd(), f.path)));
+        }));
 
     gulp.src(cssFiles)
-        .pipe(stylus({ compress: true, use: nib() }))
+        .pipe(stylus({compress: true, use: nib()}))
         .pipe(concat('blob.css'))
-        .pipe(gulp.dest('./dist/css'))
+        .pipe(gulp.dest('./dist/css'));
 
     return browserify('./new_src/js/app.js')
         .transform(babelify, {presets: ["es2015"]})
@@ -39,23 +46,23 @@ gulp.task('browserify', ['views'], function() {
         .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('html', function() {
+gulp.task('html', function () {
     return gulp.src("new_src/index.pug")
-        .pipe(pug({ locals: { mode: 'production', pkg: pack } }))
+        .pipe(pug({locals: {mode: 'production', pkg: pack}}))
         .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('views', function() {
+gulp.task('views', function () {
 
     return gulp.src(viewFiles)
-        .pipe(pug({ locals: { mode: 'production', pkg: pack } }))
+        .pipe(pug({locals: {mode: 'production', pkg: pack}}))
         .pipe(gulp.dest('./dist'))
 
 });
 
 // This task is used for building production ready
 // minified JS/CSS files into the dist/ folder
-gulp.task('build', ['html', 'browserify'], function() {
+gulp.task('build', ['html', 'browserify'], function () {
     var html = gulp.src("build/index.html")
         .pipe(gulp.dest('./dist/'));
 
@@ -63,10 +70,10 @@ gulp.task('build', ['html', 'browserify'], function() {
         .pipe(uglify())
         .pipe(gulp.dest('./dist/'));
 
-    return merge(html,js);
+    return merge(html, js);
 });
 
-gulp.task('default', ['html', 'browserify'], function() {
+gulp.task('default', ['html', 'browserify'], function () {
 
     browserSync.init(['./dist/**/**.**'], {
         server: "./dist",
--- new_src/js/services/index.js
+++ new_src/js/services/index.js
@@ -1,6 +1,5 @@
 import angular from 'angular';
 
-// Create the module where our functionality can attach to
 let servicesModule = angular.module('app.services', []);
 
 import MarketsService from './markets.service';
@@ -9,7 +8,7 @@ servicesModule.service('Markets', MarketsService);
 import CoinMarketCap from './coinmarketcap.service';
 servicesModule.service('CoinMarketCap', CoinMarketCap);
 
-import GasPrice from './GasPrice.service';
+import GasPrice from './gasprice.service';
 servicesModule.service('GasPrice', GasPrice);
 
 export default servicesModule;
