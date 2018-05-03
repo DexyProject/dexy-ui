9681a2aa18451a2351f8e4c3d00ef4d703b4f29f
Ivo Georgiev <ivo@strem.io>
2018-05-03 15:16:33 +0800

    Configs: update tokens

--- configs/mainnet.js
+++ configs/mainnet.js
@@ -13,5 +13,5 @@ module.exports = {
     warningMsg: 'Exchange is configured for Ethereum mainnet, please switch to mainnet.',
 
     tokens: require('./tokens/index'),
-    markets: ["EOS","TRX","USDT","QTUM","OMG","ICX","BNB","DGD","PPT","MKR","SNT","AE","REP","ZRX","VERI","BTM","DRGN","BAT","GNT","ZIL","LRC","AION","ELF","FUN","KNC","SALT","KIN","DENT","POWR","PAY","DCN","CND","BNT","PLR","ICN","REQ","POLY","SMART","AGI","GNO","WAX","QSP","GVT","ENJ","STORJ","RDN","SAN","MCO","CVC","POE","MANA","SUB","STORM","ANT","RLC","VEE","PPP","DTR","MTL","ADX","PRL","TEL","ITC","OST","AMB","NULS","EDG","WPR","SPANK","QRL","C20","EDO","SNM","MLN","WINGS","JNT","AST","APPC","SNGLS","FUEL","CMT","COB","INS","SRN","MGO","UTK"],
+    markets: ["USDT","QTUM","OMG","ICX","BNB","DGD","PPT","MKR","SNT","AE","REP","ZRX","VERI","BTM","DRGN","BAT","GNT","ZIL","LRC","AION","ELF","FUN","KNC","SALT","KIN","DENT","POWR","PAY","DCN","CND","BNT","PLR","ICN","REQ","POLY","SMART","AGI","GNO","WAX","QSP","GVT","ENJ","STORJ","RDN","SAN","MCO","CVC","POE","MANA","SUB","STORM","ANT","RLC","VEE","PPP","DTR","MTL","ADX","PRL","TEL","ITC","OST","AMB","NULS","EDG","WPR","SPANK","QRL","C20","EDO","SNM","MLN","WINGS","JNT","AST","APPC","SNGLS","FUEL","CMT","COB","INS","SRN","MGO","UTK"],
 }
